9bc4cf133f663f6360d06c8c2624981db4aece41
aliskhanoff <sociprogrammer@gmail.com>
2024-10-28 21:11:32 +0100

    fix: sometimes need to parse a string to number

--- apps/backend/src/generators/invite-ticket.ts
+++ apps/backend/src/generators/invite-ticket.ts
@@ -5,7 +5,7 @@ export const generateInviteTicket = (length = 6) => {
   const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz0123456789';
   const charsLength = chars.length;
 
-  const random_code = randomBytes(length);
+  const random_code = randomBytes(Number(length));
   let inviteCode = '';
 
   for (let i = 0; i < length; i++) inviteCode += chars[random_code[i] % charsLength]
